4b44ac54b1efff9abaa8679a953b4ce36be6ab6a
sjgmojica <mojicasarahjane@gmail.com>
2015-09-18 17:43:38 +0800

    It contains files for CRUD operations of apps

---
+++ controllers/blog.js
@@ -0,0 +1,101 @@
+var mongoose = require('mongoose');
+
+// Save new blog post to blogs collections
+var addBlog = function(req, res) {
+    //res.send(req.session.user._id);
+    mongoose.model('Blog').create({
+        title : req.body.blogTitle,
+        body : req.body.blogBody,
+        date : req.body.date,
+        author : req.body.author,
+        userId : req.session.user._id
+    }, function (err, user) {
+        if (err) {
+            console.log("There was a problem adding new blog to database :: " + err);
+            res.send("There was a problem adding new blog to database :: " + err);
+        } else {
+            console.log("POST new users ::" + user);
+            req.session.message = 'Successfully added blog';
+            res.redirect('/blog');
+        }
+    });
+}
+
+// Fetch all the blog data from blogs collections
+var blogList = function(req, res, cbblog) {
+	mongoose.model('Blog').find({userId : req.session.user._id}, function(err, blog) {
+        if (err) {
+            console.log("GET error: problem on retirieving blog id :: " + err);
+            res.send("There was a problem retrieving blog id :: " + err);
+        } else {
+			cbblog(blog);
+        }
+    });
+}
+
+// Find blog by id from blogs collections.If exists, the fetch data will display to edit page
+var getBlogById = function(req, res, cbblogOne) {
+    console.log("here")
+    mongoose.model('Blog').findById(req.param('blog_id'), function(err, blog) {
+        console.log("there")
+        if (err) {
+            console.log("Get blog by id :: " + err);
+            res.send("Get blog by id ::" + err);
+        } else {
+            console.log(blog);
+            res.render('blog/edit', { blog: blog });
+        }
+    });
+}
+
+// Find blog by id from blogs collections.After updating the data it will redirect to /blog or view home page
+var updateBlog = function(req, res) {
+    mongoose.model('Blog').findById(req.param('blog_id'), function(err, blog) {
+        if (err) {
+            console.log("PUT blog by id. error on find by id :: " + err);
+            res.send("PUT blog by id. error on find by id :: " + err);
+        } else {
+            blog.update({
+            title : req.body.blogBody,
+            body : req.body.blogTitle,
+            date : req.body.date,
+            author : req.body.author,
+            userId : req.session.user._id    
+            }, function(err, blogId) {
+                if (err) {
+                    console.log("PUT blog by id ::" + err);
+                    res.send("PUT blog by id :: " + err);
+                } else {
+                    console.log("Successfully updated blog by id then redirect to /blog");
+                    res.redirect('/blog');
+                }
+            });
+        } 
+    });
+}
+
+// Find blog by id from blogs collections.After deleting the data it will redirect to /blog or view home page
+var deleteBlog = function(req, res) {
+    mongoose.model('Blog').findById(req.param('blog_id'), function(err, blog) {
+        if (err) {
+            console.log("DELETE blog by id. error on find by id :: " + err);
+            res.send("DELETE blog by id. error on find by id :: " + err);
+        } else {
+            blog.remove(function(err, blogId) {
+                if (err) {
+                    console.log("DELETE blog by id ::" + err);
+                    res.sender("DELETE blog by id ::" + err);
+                } else {
+                    console.log("Successfully deletes blog by id :: " + blog._id);
+                    res.redirect('/blog');
+                }
+            });    
+        }
+    });
+}
+
+exports.addBlog = addBlog;
+exports.blogList = blogList;
+exports.getBlogById = getBlogById;
+exports.updateBlog = updateBlog;
+exports.deleteBlog = deleteBlog;
---
+++ controllers/user.js
@@ -0,0 +1,87 @@
+var mongoose = require('mongoose');
+
+// Find username and password from users collections. If valid redirect to /blog or view home page
+var login = function(req, res) {
+	mongoose.model('User').findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+        if (err) {
+            console.log("There was a problem retrieving email and password:: " + err);
+            return handleError(err);
+        } else {
+            console.log("check db return :: "+ user);
+            if (!user) {
+                console.log('Invalid username or password');
+                res.render('login', { message: 'Invalid username or password' });
+            } else  {
+                if (user.email === req.body.email && user.password === req.body.password) {
+                    req.session.user = user;
+                    console.log(req.session);
+                    console.log('Valid username and password');
+                    res.redirect('/blog');
+                }
+            }
+        }
+    });
+}
+
+// Add new users. Upon successful registation it will redirect to /login or login page
+var register = function(req, res) {
+    mongoose.model('User').create({
+        firstname : req.body.firstname,
+        lastname  : req.body.lastname,
+        email     : req.body.email,
+        password  : req.body.password
+    }, function (err, user) {
+        if (err) {
+            console.log("There was a problem adding new users to database :: " + err);
+            res.send("There was a problem adding new users to database :: " + err);
+        } else {
+            console.log("POST new users ::" + user);
+            req.flash('login', 'Successfully added user');
+            res.redirect('/login');
+        }
+    });
+}
+
+// Find users data by id from users collections then render the edit view page 
+var getProfile = function(req, res) {
+    console.log("=====");
+    console.log(req.param('id'));
+    console.log("=====");
+    mongoose.model('User').findById(req.param('id'), function(err, user) {
+        if (err) {
+            console.log("GET error: problem on retirieving users id heheheh:: " + err);
+            res.send("There was a problem retrieving users id heheheheh:: " + err);
+        } else {
+            console.log("GET id :: Update :: " + user._id);
+            res.render('edit', { user: user })
+        }
+    });
+}
+
+// Find users profile from users collections and update the data then redirect to /blog page
+var updateProfile = function(req, res) {
+    mongoose.model('User').findById(req.param('id'), function(err, user) {
+        user.update({
+            firstname : req.body.firstname,
+            lastname  : req.body.lastname,
+            email     : req.body.email,
+            password  : req.body.password
+        }, function(err, userId){
+            if (err) {
+                console.log("PUT error :: Update :: " + err);
+                res.send("There was a problem updating the info to the blogDB :: " + err);
+            } else {
+                console.log("Successfully update profile");
+                console.log(req.body.firstname);
+                req.session.user.firstname = req.body.firstname;
+                req.session.message = 'Successfully updated profile';
+                res.redirect('/blog');
+            }
+        })
+    });
+}
+
+exports.login = login;
+exports.register = register;
+exports.getProfile = getProfile;
+exports.updateProfile = updateProfile;
