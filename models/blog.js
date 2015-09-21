e347dafa4f79baf6f90816f2010133f2ed8d098d
sjgmojica <mojicasarahjane@gmail.com>
2015-09-21 19:49:50 +0800

    change time to deafult date now

--- models/blog.js
+++ models/blog.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var blogSchema = new mongoose.Schema({
 	title  : String,
 	body   : String,
-	date   : String,
+	date   : {type: Date, default: Date.now},
 	author : String,
 	userId : String
 });
