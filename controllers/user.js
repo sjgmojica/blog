290b55e4528bb9761028b77f823b806454f6aa97
sjgmojica <mojicasarahjane@gmail.com>
2015-09-22 18:18:08 +0800

    fix some mongoose crud

--- controllers/user.js
+++ controllers/user.js
@@ -4,18 +4,14 @@ var mongoose = require('mongoose');
 var login = function(req, res) {
 	mongoose.model('User').findOne({email: req.body.email, password: req.body.password}, function(err, user) {
         if (err) {
-            console.log("There was a problem retrieving email and password:: " + err);
-            return handleError(err);
+            res.send("login error" + err);
         } else {
             console.log("check db return :: "+ user);
             if (!user) {
-                console.log('Invalid username or password');
                 res.render('login', { message: 'Invalid username or password' });
             } else  {
                 if (user.email === req.body.email && user.password === req.body.password) {
                     req.session.user = user;
-                    console.log(req.session);
-                    console.log('Valid username and password');
                     res.redirect('/blog');
                 }
             }
@@ -32,10 +28,8 @@ var register = function(req, res) {
         password  : req.body.password
     }, function (err, user) {
         if (err) {
-            console.log("There was a problem adding new users to database :: " + err);
             res.send("There was a problem adding new users to database :: " + err);
         } else {
-            console.log("POST new users ::" + user);
             res.render('login', { message: 'Welcome to Blog App. Please login to your page'});
         }
     });
@@ -45,11 +39,9 @@ var register = function(req, res) {
 var getProfile = function(req, res) {
     mongoose.model('User').findById(req.param('id'), function(err, user) {
         if (err) {
-            console.log("GET error: problem on retirieving users id heheheh:: " + err);
             res.send("There was a problem retrieving users id heheheheh:: " + err);
         } else {
-            console.log("GET id :: Update :: " + user._id);
-            res.render('edit', { user: user })
+            res.render('edit', { user: user });
         }
     });
 }
@@ -64,11 +56,8 @@ var updateProfile = function(req, res) {
             password  : req.body.password
         }, function(err, userId){
             if (err) {
-                console.log("PUT error :: Update :: " + err);
                 res.send("There was a problem updating the info to the blogDB :: " + err);
             } else {
-                console.log("Successfully update profile");
-                console.log(req.body.firstname);
                 req.session.user.firstname = req.body.firstname;
                 req.session.message = 'Successfully updated profile';
                 res.redirect('/blog');
