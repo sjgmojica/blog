5165107eb3689aaf98c8bc21dec96193da21eed4
sjgmojica <mojicasarahjane@gmail.com>
2015-09-22 18:13:25 +0800

    add a function that fetch data by specific user id

--- controllers/blog.js
+++ controllers/blog.js
@@ -10,10 +10,8 @@ var addBlog = function(req, res) {
         userId : req.session.user._id
     }, function (err, user) {
         if (err) {
-            console.log("There was a problem adding new blog to database :: " + err);
             res.send("There was a problem adding new blog to database :: " + err);
         } else {
-            console.log("POST new users ::" + user);
             req.session.message = 'Successfully added blog';
             res.redirect('/blog');
         }
@@ -24,7 +22,17 @@ var addBlog = function(req, res) {
 var blogList = function(req, res, cbblog) {
     mongoose.model('Blog').find({}, function(err, blog) {
         if (err) {
-            console.log("GET error: problem on retirieving blog id :: " + err);
+            res.send("There was a problem retrieving blog id :: " + err);
+        } else {
+            cbblog(blog);
+        }
+    });
+}
+
+// Fetch all the blog data by userID from blogs collections
+var blogListById = function(req, res, cbblog) {
+    mongoose.model('Blog').find({userId: req.session.user._id}, function(err, blog) {
+        if (err) {
             res.send("There was a problem retrieving blog id :: " + err);
         } else {
             cbblog(blog);
@@ -35,12 +43,9 @@ var blogList = function(req, res, cbblog) {
 // Find blog by id from blogs collections.If exists, the fetch data will display to edit page
 var getBlogById = function(req, res, cbblogOne) {
     mongoose.model('Blog').findById(req.param('blog_id'), function(err, blog) {
-        console.log("there")
         if (err) {
-            console.log("Get blog by id :: " + err);
             res.send("Get blog by id ::" + err);
         } else {
-            console.log(blog);
             res.render('blog/edit', { blog: blog });
         }
     });
@@ -50,21 +55,18 @@ var getBlogById = function(req, res, cbblogOne) {
 var updateBlog = function(req, res) {
     mongoose.model('Blog').findById(req.param('blog_id'), function(err, blog) {
         if (err) {
-            console.log("PUT blog by id. error on find by id :: " + err);
             res.send("PUT blog by id. error on find by id :: " + err);
         } else {
             blog.update({
-            title : req.body.blogBody,
-            body : req.body.blogTitle,
+            title : req.body.blogTitle,
+            body : req.body.blogBody,
             date : req.body.date,
             author : req.body.author,
             userId : req.session.user._id    
             }, function(err, blogId) {
                 if (err) {
-                    console.log("PUT blog by id ::" + err);
                     res.send("PUT blog by id :: " + err);
                 } else {
-                    console.log("Successfully updated blog by id then redirect to /blog");
                     res.redirect('/blog');
                 }
             });
@@ -76,15 +78,12 @@ var updateBlog = function(req, res) {
 var deleteBlog = function(req, res) {
     mongoose.model('Blog').findById(req.param('blog_id'), function(err, blog) {
         if (err) {
-            console.log("DELETE blog by id. error on find by id :: " + err);
             res.send("DELETE blog by id. error on find by id :: " + err);
         } else {
             blog.remove(function(err, blogId) {
                 if (err) {
-                    console.log("DELETE blog by id ::" + err);
                     res.sender("DELETE blog by id ::" + err);
                 } else {
-                    console.log("Successfully deletes blog by id :: " + blog._id);
                     res.redirect('/blog');
                 }
             });    
@@ -97,3 +96,4 @@ exports.blogList = blogList;
 exports.getBlogById = getBlogById;
 exports.updateBlog = updateBlog;
 exports.deleteBlog = deleteBlog;
+exports.blogListById = blogListById;
